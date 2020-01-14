bcedcc9fa38e016c133a50c540d8f91fa11997fc
Matthew-SA <surfsnow1@gmail.com>
2020-01-14 10:29:23 -0800

    Username displaying in chat

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -3,10 +3,13 @@ class ChatChannel < ApplicationCable::Channel
     stream_for 'chat_channel'
   end
   def speak(data)
-    current_user(data['user_id'], data['session_token'])
-    message = Message.new(body: data['message'])
+    # current_user(data['user_id'], data['session_token'])
+    message = Message.new(body: data['message'], user_id: data['user_id'])
+    user = User.find_by(id: data['user_id'])
+    # debugger
     if message.save
-      socket = { message: message.body, type: 'message' }
+      socket = { message: ((user.username)+ ': ' + message.body), type: 'message' }
+      # socket = { message: ((message.user_id).to_s + ': ' + message.body), type: 'message' }
       ChatChannel.broadcast_to('chat_channel', socket)
     end
   end
