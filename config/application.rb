7cdeb3d4da78bc5ac675459a26d359970b86c2b8
Matthew-SA <surfsnow1@gmail.com>
2020-04-17 21:56:00 -0700

    set server timezone

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,6 @@ module FullStackSlack
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    # config.time_zone = "Pacific Time (US & Canada)"
+    config.time_zone = "Pacific Time (US & Canada)"
   end
 end
