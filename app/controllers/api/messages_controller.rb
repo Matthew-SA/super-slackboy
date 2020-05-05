03187a3ef44ff2ce2a4cbd66873207b869f19fbb
Matthew-SA <surfsnow1@gmail.com>
2020-05-05 14:16:14 -0700

    fix n+1 on message index load

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::MessagesController < ApplicationController
 
   def index
     channels = current_user.channels.ids
-    @messages = Message.all.where(channel_id: channels)
+    @messages = Message.all.includes(:user).where(channel_id: channels)
     render '/api/messages/index'
   end
 
