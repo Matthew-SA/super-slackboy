6bfaeb8d4cd986832ed094d120342cc9036114e7
Matthew-SA <surfsnow1@gmail.com>
2020-05-08 12:54:35 -0700

    update linkedin link

--- frontend/components/home/Home.jsx
+++ frontend/components/home/Home.jsx
@@ -48,7 +48,7 @@ const Home = () => (
     <footer className="home-footer">
       <div className="first-subfooter">
         <ul className="social-icons">
-          <li><a href="https://www.linkedin.com/in/matthew-andresen-ab8a5b191/"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a></li>
+          <li><a href="https://www.linkedin.com/in/matthew-andresen/"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a></li>
           {/* <li><a href="https://angel.co/u/matthew-andresen"><FontAwesomeIcon icon={['fab', 'angellist']} /></a></li> */}
           <li><a href="https://github.com/Matthew-SA"><FontAwesomeIcon icon={['fab', 'github-square']} /></a></li>
           <li><a href="https://matthewandresen.com"><FontAwesomeIcon icon="briefcase" /></a></li>
