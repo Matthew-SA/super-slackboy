d886be24f3c86a32bc0833f0b16327c4b9afe794
Matthew-SA <surfsnow1@gmail.com>
2020-05-15 22:55:12 -0700

    fix schema bug

--- db/migrate/20200506164440_add_last_accessed_to_memberships.rb
+++ db/migrate/20200506164440_add_last_accessed_to_memberships.rb
@@ -1,5 +1,5 @@
 class AddLastAccessedToMemberships < ActiveRecord::Migration[5.2]
   def change
-    add_column :memberships, :last_arrived, :datetime
+    add_column :memberships, :last_accessed, :datetime
   end
 end
